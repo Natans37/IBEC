110950002c772cb76684291e3edd975a9ef69521
matheuscs0 <dev.matheuscs@gmail.com>
2025-03-06 15:51:58 -0300

    feat: repository autor

--- src/main/java/com/ibec_api/domain/dto/LivroReqDTO.java
+++ src/main/java/com/ibec_api/domain/dto/LivroReqDTO.java
@@ -2,7 +2,6 @@ package com.ibec_api.domain.dto;
 
 import com.ibec_api.domain.entity.Autor;
 
-import java.util.UUID;
 
 public record LivroReqDTO(
         String nome,
---
+++ src/main/java/com/ibec_api/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.ibec_api.repository;
+
+import com.ibec_api.domain.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
